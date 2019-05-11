86900ea4c7257ea467efe18b7de7b454cb8f259b
Ilia Silakov <me@ileamare.ru>
2019-05-11 22:36:36 +0300

    add processAllPlayers

---
+++ processors/processAllPlayers.php
@@ -0,0 +1,43 @@
+<?php 
+
+namespace \CreateParsedDataBlob;
+
+include __DIR__ . "../util/utility.php";
+
+/**
+ * Compute data requiring all players in a match for storage in match table
+ * */
+function processAllPlayers($entries, $meta) {
+  $goldAdvTime = [];
+  $xpAdvTime = [];
+  $res = [
+    'radiant_gold_adv' => [],
+    'radiant_xp_adv' => [],
+  ];
+
+  foreach ($entries as $e) {
+    if ($e['time'] >= 0 && $e['time'] % 60 === 0 && $e['type'] === 'interval') {
+      $g = \odota\core\utils\isRadiant([
+        'player_slot' => $meta['slot_to_playerslot'][$e['slot']],
+      ]) ? $e['gold'] : -$e['gold'];
+      $x = \odota\core\utils\isRadiant([
+        'player_slot' => $meta['slot_to_playerslot'][$e['slot']],
+      ]) ? $e['xp'] : -$e['xp'];
+      $goldAdvTime[$e['time']] = $goldAdvTime[$e['time']] ? $goldAdvTime[$e['time']] + $g : $g;
+      $xpAdvTime[$e['time']] = $xpAdvTime[$e['time']] ? $xpAdvTime[$e['time']] + $x : $x;
+    }
+  }
+
+  $order = array_keys($goldAdvTime);
+  usort($order, function($a, $b) {
+    return (int)$a - (int)$b;
+  });
+  foreach ($order as $k) {
+    $res['radiant_gold_adv'][] = $goldAdvTime[$k];
+    $res['radiant_xp_adv'][] = $xpAdvTime[$k];
+  }
+
+  return $res;
+}
+
+?>

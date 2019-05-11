27895ac922cdb77d0c246e8f263230a9d3f15750
Ilia Silakov <me@ileamare.ru>
2019-05-12 02:42:42 +0300

    add processParsedData

---
+++ processors/processParsedData.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace \CreateParsedDataBlob;
+
+include_once __DIR__ . "populate.php";
+
+function processParsedData($entries, $container, $meta) {
+  foreach ($entries as $e) {
+    populate($e, $container, $meta);
+  }
+
+  return $container;
+}
+
+?>
